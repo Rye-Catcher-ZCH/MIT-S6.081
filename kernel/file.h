b41974847d783890a489dc3a30de157203085e5d
Rye-Catcher-ZCH <zch295153721@gmail.com>
2019-12-22 04:01:14 -0500

    finish bigfile

--- kernel/file.h
+++ kernel/file.h
@@ -27,7 +27,7 @@ struct inode {
   short minor;
   short nlink;
   uint size;
-  uint addrs[NDIRECT+1];
+  uint addrs[NDIRECT+1+1];  //NDIRECT has been changed in fs.h so here should be changed ref.hint 4
 };
 
 // map major device number to device functions.
--- kernel/fs.c
+++ kernel/fs.c
@@ -366,6 +366,9 @@ iunlockput(struct inode *ip)
   iput(ip);
 }
 
+
+
+
 // Inode content
 //
 // The content (data) associated with each inode is stored
@@ -375,70 +378,188 @@ iunlockput(struct inode *ip)
 
 // Return the disk block address of the nth block in inode ip.
 // If there is no such block, bmap allocates one.
+// 传入一个文件的inode,inode->addrs[]为i_blocks
+// inode->addrs[NDIRECT]为一级间接索引的指针
+// inode->addrs[NINDRIECT]为二级间接索引指针
+// bmap返回inode第n个磁盘块的地址，如果没有，且访问在合法范围内，则bmap创建一个块
+// inode 定义在file.h中，具体如下
+// in-memory copy of an inode
+/*
+struct inode {
+  uint dev;           // Device number
+  uint inum;          // Inode number
+  int ref;            // Reference count
+  struct sleeplock lock; // protects everything below here
+  int valid;          // inode has been read from disk?
+
+  short type;         // copy of disk inode
+  short major;
+  short minor;
+  short nlink;
+  uint size;
+  uint addrs[NDIRECT+1];
+};*/
 static uint
-bmap(struct inode *ip, uint bn)
+bmap(struct inode *ip, uint bn) //传入文件inode和返回的块号bn
 {
-  uint addr, *a;
-  struct buf *bp;
+    uint addr, *a;
+    struct buf *bp; //缓冲区
 
-  if(bn < NDIRECT){
-    if((addr = ip->addrs[bn]) == 0)
-      ip->addrs[bn] = addr = balloc(ip->dev);
-    return addr;
-  }
-  bn -= NDIRECT;
-
-  if(bn < NINDIRECT){
-    // Load indirect block, allocating if necessary.
-    if((addr = ip->addrs[NDIRECT]) == 0)
-      ip->addrs[NDIRECT] = addr = balloc(ip->dev);
-    bp = bread(ip->dev, addr);
-    a = (uint*)bp->data;
-    if((addr = a[bn]) == 0){
-      a[bn] = addr = balloc(ip->dev);
-      log_write(bp);
+    if (bn < NDIRECT) //如果块号<NDIRECT(11)为直接索引
+    {
+        if ((addr = ip->addrs[bn]) == 0)            //直接通过ip->addrs[bn]找到块号
+            ip->addrs[bn] = addr = balloc(ip->dev); //如果为0，说明未分配，则调用balloc函数分配磁盘块空间，逻辑和物理地址的映射记录在inode的addrs(i_block)中
+        return addr;
     }
-    brelse(bp);
-    return addr;
-  }
+    bn -= NDIRECT; //bn-11，eg:bn = 260 - 11 < 256
 
-  panic("bmap: out of range");
+    if (bn < NINDIRECT) //如果bn小于256
+    {
+        // Load indirect block, allocating if necessary.
+        // 加载一级间接索引表，如果没有，则分配，一级间接索引表也占据一个磁盘块的大小
+        if ((addr = ip->addrs[NDIRECT]) == 0)
+            ip->addrs[NDIRECT] = addr = balloc(ip->dev); //ip->dev为设备号，应该用来定位磁盘空间
+        /*
+           ref. xv6_book 7.3
+           Bread (kernel/bio.c:91) calls bget to get a buffer for the given sector (kernel/bio.c:95). 
+           If the buffer needs to be read from disk, bread calls virtio_disk_rw to do that before returning the buffer
+        */
+        bp = bread(ip->dev, addr); //获得缓冲区
+        a = (uint *)bp->data;      //获得间接索引磁盘块指针指向的数据,ref. xv6_book page 84 figure 7.3
+        if ((addr = a[bn]) == 0)   //如果数据块为0 ，说明未分配，则调用balloc分配
+        {
+            a[bn] = addr = balloc(ip->dev);
+            //ref. xv6_book page 80
+            //log_write将块的扇区号记录在内存中，在磁盘上的日志中保留一个插槽(slot)，并将缓冲区固定在块缓存中，以防止块缓存逐出它。
+            log_write(bp);
+        }
+        //ref. xv6_book page 78
+        //Brelse (kernel/bio.c:115) releases the sleep-lock and moves the buffer to the front of the linked list (kernel/bio.c:126-131).
+        brelse(bp);
+        return addr;
+    }
+
+    bn -= NINDIRECT; //eg:bn = 1000 - 256 - 11 < 65536
+    if (bn < NININDIRECT)
+    {
+        uint index_table_num = 0;
+        uint index_item_num = 0;
+        //首先找到第bn个数据块属于哪个间接索引表
+        index_table_num = bn / NINDIRECT; //eg: 第1000个块，1000-11-256 = 733，733/256 = 2.86 = 2，所以在第3个间接索引表中
+        //然后找到属于第3个表中的那一个索引项
+        index_item_num = bn % NINDIRECT; //733除256的余数，就是它在第3个索引表中的索引。
+
+        // 加载二级间接索引表，如果没有，则分配，二级间接索引表也占据一个磁盘块的大小
+        if ((addr = ip->addrs[NDIRECT + 1]) == 0)
+            ip->addrs[NDIRECT + 1] = addr = balloc(ip->dev); //ip->dev为设备号，应该用来定位磁盘空间
+
+        // 加载间接索引表，此时addr指向最大的索引表，addr->data是一个索引表
+        bp = bread(ip->dev, addr); //获得缓冲区
+        a = (uint *)bp->data;      //获得间接索引磁盘块指针指向的数据,ref. xv6_book page 84 figure 7.3
+        // 如果间接索引表不存在，则分配
+        if((addr = a[index_table_num]) == 0)  
+        {
+            a[index_table_num] = addr = balloc(ip->dev);
+            log_write(bp);
+        }
+        brelse(bp);  //释放缓存区，该缓存区是存储某个间接索引表的磁盘块的缓存区，现在addr已经指向该表
+        
+        // 此时addr指向间接索引表，addr->data是存储数据的磁盘块
+        bp = bread(ip->dev, addr); //获得addr所指内存的缓冲区
+        a = (uint *)bp->data;      //获得间接索引磁盘块指针指向的数据,ref. xv6_book page 84 figure 7.3
+
+        //如果存储数据的磁盘块不存在，则分配
+        if((addr = a[index_item_num]) == 0)
+        {
+            a[index_item_num] = addr = balloc(ip->dev);
+            log_write(bp);
+        }
+        brelse(bp);
+        return addr;  //返回addr，addr指向存储数据的磁盘块，是第bn个磁盘块的地址
+    }
+    panic("bmap: out of range");
 }
+
+
+
 
 // Truncate inode (discard contents).
 // Only called when the inode has no links
 // to it (no directory entries referring to it)
 // and has no in-memory reference to it (is
 // not an open file or current directory).
+/*
+截断索引节点（丢弃内容）。
+仅在索引节点没有链接（没有引用索引的目录条目）并且没有内存引用（不是打开的文件或当前目录）时调用。
+*/
+
+// 这里需要修改，要确保itrunc释放文件的所有块，包括双间接块。ref. hint8
+// 这里不修改可能会影响write big的样例
 static void
 itrunc(struct inode *ip)
 {
-  int i, j;
-  struct buf *bp;
-  uint *a;
+    int i, j;
+    struct buf *bp, *bp_child; //bp_child存储间接索引表
+    uint *a;
+    uint *index_table_items;
 
-  for(i = 0; i < NDIRECT; i++){
-    if(ip->addrs[i]){
-      bfree(ip->dev, ip->addrs[i]);
-      ip->addrs[i] = 0;
+    // 释放直接索引的磁盘块空间
+    for (i = 0; i < NDIRECT; i++)
+    {
+        if (ip->addrs[i])
+        {
+            bfree(ip->dev, ip->addrs[i]);
+            ip->addrs[i] = 0;
+        }
     }
-  }
 
-  if(ip->addrs[NDIRECT]){
-    bp = bread(ip->dev, ip->addrs[NDIRECT]);
-    a = (uint*)bp->data;
-    for(j = 0; j < NINDIRECT; j++){
-      if(a[j])
-        bfree(ip->dev, a[j]);
+    // 释放一级间接索引的磁盘块空间
+    if (ip->addrs[NDIRECT]) // 如果inode有一级间接索引表(ip->addrs[NDIRECT]不为0)
+    {
+        bp = bread(ip->dev, ip->addrs[NDIRECT]); //获得ip->addrs[NDIRECT]磁盘块内容到缓冲区bp
+        a = (uint *)bp->data;
+        for (j = 0; j < NINDIRECT; j++) //遍历一级间接索引表中的每个item(指向存储数据data的磁盘块)
+        {
+            if (a[j])                 //如果磁盘块里面有存储数据,a[j] != 0
+                bfree(ip->dev, a[j]); //将其释放
+        }
+        brelse(bp);                         //释放缓冲区bp
+        bfree(ip->dev, ip->addrs[NDIRECT]); //释放一级间接索引表
+        ip->addrs[NDIRECT] = 0;             //指针置为0
     }
-    brelse(bp);
-    bfree(ip->dev, ip->addrs[NDIRECT]);
-    ip->addrs[NDIRECT] = 0;
-  }
 
-  ip->size = 0;
-  iupdate(ip);
+    if (ip->addrs[NDIRECT + 1]) //如果inode有二级间接索引表
+    {
+        bp = bread(ip->dev, ip->addrs[NDIRECT + 1]); //获得ip->addrs[NDIRECT+1]磁盘块内容到缓冲区bp
+        a = (uint *)bp->data;
+        for (i = 0; i < NDIRECT; i++)
+        {
+            if (a[i]) //如果有间接索引表
+            {
+                bp_child = bread(ip->dev, a[i]); //获得a[i]磁盘块内容到缓冲区bp_child,这是一个指向间接索引表的指针
+                index_table_items = (uint *)bp_child->data;
+                for (j = 0; j < NDIRECT; j++)
+                {
+                    if (index_table_items[j]) //如果有指向存储数据data的磁盘块
+                        bfree(ip->dev, index_table_items[j]);
+                }
+                brelse(bp_child); //释放缓冲区bp_child
+                bfree(ip->dev, a[i]);      //清空该间接索引表
+            }
+        }
+        brelse(bp);                             //释放缓冲区bp
+        bfree(ip->dev, ip->addrs[NDIRECT + 1]); //释放最外层索引表
+        ip->addrs[NDIRECT + 1] = 0;
+    }
+    ip->size = 0;
+    iupdate(ip);
 }
+
+
+
+
+
+
 
 // Copy stat information from inode.
 // Caller must hold ip->lock.
